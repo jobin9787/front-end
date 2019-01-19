e96b32af470bfc13c93f6c871426c7a5d7c8abed
Andre Jobin <jobin9787@yahoo.fr>
2019-01-19 16:40:48 -0500

    car detail

--- src/app/models/carad.ts
+++ src/app/models/carad.ts
@@ -1,27 +1,27 @@
 export class Carad {
 
 	  public  id: String;
-    public  make: String;
-		public  body: String;
-		public  model: String;
-		public  type: String;
+    public  make: string;
+		public  body: string;
+		public  model: string;
+		public  type: string;
 		public  year: number;
 		public  price: number;
 		public  kilometers: number;
 		public  fuel: String ;
 		public  iduser: String;
-		public  adtitle: String;
+		public  adtitle: string;
 		public  descr: String;
 		public  adlocation: String;
 		public  address: String;
 		public  phone: String;
 		public  email: String;
 		public  adtype: String;
-		public  soldby: String;
+		public  soldby: string;
 		public  trans: String;
-		public  color: String;
-		public  drivetrain: String;
-		public  doorNo: String;
+		public  color: string;
+		public  drivetrain: string;
+		public  doorNo: string;
     public  fileNumber: number;
     public  postalcode: String;
 }
