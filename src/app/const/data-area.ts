d8b6b74f1d21b6e239cafaed20fb915f4a11fa2e
Andre Jobin <jobin9787@yahoo.fr>
2019-01-24 08:06:44 -0500

    add area section

--- src/app/const/data-area.ts
+++ src/app/const/data-area.ts
@@ -2,12 +2,20 @@ export class AreaCode {
 
 
 public static areaCodeMap = new Map([
-["en", [{value:"H1A",name:"Pointe-aux-Trembles"},{value:"H1B",name:"Montréal-Est"},{value:"H1C",name:"Rivière-des-Prairies"},{value:"H1G",name:"Montréal-Nord"},{value:"H1P",name:"Saint-Léonard"},{value:"H2A",name:"Saint-Michel"}, {value:"H3A",name:"Centre-ville de Montréal"},
-{value:"H4A",name:"Notre-Dame-de-Grâce"},{value:"H8Y",name:"Roxboro"}, {value:"H7V",name:"Chomedey"},{value:"H7Y",name:"les-Laval"},{"",name:""},{value:"undefined",name:""},{value:"other",name:""}]
-
-//["fr",new Map([["beig","Beige"],["blac","Noir"],["blue","Bleu"],["brow","Brun"],["gold","Or"],["gree","Vert"], ["grey","Gris"],
-//["red","Rouge"],["silve","Argent"], ["whit","Blanc"],["null",""],["",""],["undefined",""],["other",""] ])]
-
-);
+["en", [{value:"H1A",name:"Pointe-aux-Trembles"},
+{value:"H1B",name:"Montréal-Est"},
+{value:"H1C",name:"Rivière-des-Prairies"},
+{value:"H1G",name:"Montréal-Nord"},
+{value:"H1P",name:"Saint-Léonard"},
+{value:"H2A",name:"Saint-Michel"},
+{value:"H3A",name:"Centre-ville de Montréal"},
+{value:"H4A",name:"Notre-Dame-de-Grâce"},
+{value:"H8Y",name:"Roxboro"},
+{value:"H7V",name:"Chomedey"},
+{value:"H7Y",name:"les-Laval"},
+{value:"",name:""},
+{value:"undefined",name:""},
+{value:"other",name:""}]
+]);
 
 }
